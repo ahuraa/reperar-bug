a9cce3b286d630ba7c1a46066c9de05d17f2652c
zeros <wowzeros2@gmail.com>
2017-08-31 16:50:41 -0500

    update

--- src/server/scripts/Custom/zeros/npc_promo.cpp
+++ src/server/scripts/Custom/zeros/npc_promo.cpp
@@ -41,20 +41,28 @@ public:
 			}
 			pjinfo.NpcText = 800001;
 			player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "De que trata la promocion? ", GOSSIP_SENDER_MAIN, PROMO_INFO);
-			if (pjinfo.prest != 0 && pjinfo.pjlevel < 80 || pjinfo.EsGM && pjinfo.pjlevel < 80)
-				player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TRAINER, string("Recibir promo (Disponibles: " + (pjinfo.EsGM ? "Sin limite (GM)":to_string(pjinfo.prest) + " de " + to_string(pjinfo.pmax) + ", es por cuenta)")).c_str(), GOSSIP_SENDER_MAIN, PROMO_DAR);
-			else 
-				player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TALK, string("Promos disponibles: " + (pjinfo.EsGM ? "Sin limite (GM) (Debe usar otro personaje o disminuir su nivel":to_string(pjinfo.prest) + " de " + to_string(pjinfo.pmax) + (pjinfo.prest != 0 ? " (Debe usar otro personaje)" : ""))).c_str(), GOSSIP_SENDER_MAIN, 0);
+			
+			if (pjinfo.prest != 0 && pjinfo.pjlevel < 80 || pjinfo.EsGM && pjinfo.pjlevel < 80){ 
+				//player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TRAINER, string("Recibir promo (Disponibles: " + (pjinfo.EsGM ? "Sin limite (GM)":to_string(pjinfo.prest) + " de " + to_string(pjinfo.pmax) + ", es por cuenta)")).c_str(), GOSSIP_SENDER_MAIN, PROMO_DAR);
+			}
+			else {
+				//player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TALK, string("Promos disponibles: " + (pjinfo.EsGM ? "Sin limite (GM) (Debe usar otro personaje o disminuir su nivel":to_string(pjinfo.prest) + " de " + to_string(pjinfo.pmax) + (pjinfo.prest != 0 ? " (Debe usar otro personaje)" : ""))).c_str(), GOSSIP_SENDER_MAIN, 0);
+			}
 			if (pjinfo.PjConPromo && pjinfo.pjlevel == 80 || pjinfo.EsGM)
 			{
-				if(spromo->Configuraciones.PVP || pjinfo.EsGM)
-					player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TRAINER, string("Set PVP disponibles: " + (pjinfo.EsGM ? "Sin limite (GM)" : to_string(pjinfo.pvpr) + " de " + to_string(pjinfo.pvpm) + " (Por Pj)")).c_str(), GOSSIP_SENDER_MAIN, (pjinfo.EsGM ? PROMO_SET_PVP : (pjinfo.pvpr != 0 ? PROMO_SET_PVP : 0)));
-				else
-					player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TRAINER, "Promocion PVP se encuentra desactivada temporalmente", GOSSIP_SENDER_MAIN, 0);
-				if (spromo->Configuraciones.PVE || pjinfo.EsGM)
-					player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TRAINER, string("Set PVE disponibles: " + (pjinfo.EsGM ? "Sin limite (GM)" : to_string(pjinfo.pver) + " de " + to_string(pjinfo.pvem) + " (Por Pj)")).c_str(), GOSSIP_SENDER_MAIN, (pjinfo.EsGM ? PROMO_SET_PVE : (pjinfo.pver != 0 ? PROMO_SET_PVE : 0)));
-				else
-					player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TRAINER, "Promocion PVE se encuentra desactivada temporalmente", GOSSIP_SENDER_MAIN, 0);
+				if (spromo->Configuraciones.PVP || pjinfo.EsGM) {
+					//player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TRAINER, string("Set PVP disponibles: " + (pjinfo.EsGM ? "Sin limite (GM)" : to_string(pjinfo.pvpr) + " de " + to_string(pjinfo.pvpm) + " (Por Pj)")).c_str(), GOSSIP_SENDER_MAIN, (pjinfo.EsGM ? PROMO_SET_PVP : (pjinfo.pvpr != 0 ? PROMO_SET_PVP : 0)));
+				}
+				else {
+					//player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TRAINER, "Promocion PVP se encuentra desactivada temporalmente", GOSSIP_SENDER_MAIN, 0);
+				}
+				if (spromo->Configuraciones.PVE || pjinfo.EsGM) {
+					//player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TRAINER, string("Set PVE disponibles: " + (pjinfo.EsGM ? "Sin limite (GM)" : to_string(pjinfo.pver) + " de " + to_string(pjinfo.pvem) + " (Por Pj)")).c_str(), GOSSIP_SENDER_MAIN, (pjinfo.EsGM ? PROMO_SET_PVE : (pjinfo.pver != 0 ? PROMO_SET_PVE : 0)));
+
+				}
+				else {
+					//player->ADD_GOSSIP_ITEM(GOSSIP_ICON_TRAINER, "Promocion PVE se encuentra desactivada temporalmente", GOSSIP_SENDER_MAIN, 0);
+				}
 				pjinfo.NpcText = 800002;
 			}
 			if (pjinfo.AccConPromo && pjinfo.pjlevel == 80 || pjinfo.EsGM)
@@ -1610,7 +1618,121 @@ public:
 	}
 };
 
+
+class custom_CongratsOnLevel : public PlayerScript
+{
+public:
+    custom_CongratsOnLevel() : PlayerScript("custom_CongratsOnLevel") { }
+
+    void OnLevelChanged(Player* player, uint8 newLevel)
+    {
+		switch(++newLevel){
+		
+			case 10:{
+				std::ostringstream ss;
+				ss << "|cffFF0000[Level Gaming]|r WoW-Legendary el player:  " << player->GetName() << " Demuestras que puedes dar tus primeros pasos avanzando satisfactoriamente al Nivel:  " << (uint32)newLevel;
+				sWorld->SendServerMessage(SERVER_MSG_STRING, ss.str().c_str());
+			break;
+			}
+			case 20:{
+				std::ostringstream ss;
+				ss << "|cffFF0000[Level Gaming]|r WoW-Legendary el player:  " << player->GetName() << " Nuevas aventuras te esperan asi como nuevos retos en el Nivel:  " << (uint32)newLevel;
+				sWorld->SendServerMessage(SERVER_MSG_STRING, ss.str().c_str());
+				 player->LearnSpell(33388, false);
+			break;	
+			}
+			case 30:{
+				std::ostringstream ss;
+				ss << "|cffFF0000[Level Gaming]|r WoW-Legendary el player:  " << player->GetName() << " Conoces nuevos caminos y es hora de tener nuevas metas en el Nivel:  " << (uint32)newLevel;
+				sWorld->SendServerMessage(SERVER_MSG_STRING, ss.str().c_str());
+			break;	
+			}
+			case 40:{
+				std::ostringstream ss;
+				ss << "|cffFF0000[Level Gaming]|r WoW-Legendary el player:  " << player->GetName() << " Acabas de llegar a la mitad de tu aventura, demuestra que superaras todos los obstaculos en el Nivel:  " << (uint32)newLevel;
+				sWorld->SendServerMessage(SERVER_MSG_STRING, ss.str().c_str());
+				player->LearnSpell(33391, false);
+			break;	
+			}
+			case 50:{
+				std::ostringstream ss;
+				ss << "|cffFF0000[Level Gaming]|r WoW-Legendary el player:  " << player->GetName() << " Haz demostrado ser autosuficiente y capaz, acaba con todos en el Nivel:  " << (uint32)newLevel;
+				sWorld->SendServerMessage(SERVER_MSG_STRING, ss.str().c_str());
+			break;	
+			}
+			case 60:{
+				std::ostringstream ss;
+				ss << "|cffFF0000[Level Gaming]|r WoW-Legendary el player:  " << player->GetName() << " Defiende tu faccion! Lucha contra la Legión Ardiente en el Nivel:  " << (uint32)newLevel;
+				sWorld->SendServerMessage(SERVER_MSG_STRING, ss.str().c_str());
+				player->LearnSpell(34090, false);
+			break;	
+			}
+			case 70:{
+				std::ostringstream ss;
+				ss << "|cffFF0000[Level Gaming]|r WoW-Legendary el player:  " << player->GetName() <<" Haz alcanzado el Nivel: " << (uint32)newLevel  <<", Los frios vientos de Rasganorte te esperan! ";
+				sWorld->SendServerMessage(SERVER_MSG_STRING, ss.str().c_str());
+				player->LearnSpell(34091, false);
+			break;	
+			}
+			case 80:{
+				std::ostringstream ss;
+				ss << "|cffFF0000[Level Gaming]|r WoW-Legendary el player:  " << player->GetName() << " Haz alcanzado el Nivel:  " << (uint32)newLevel << "y con ello nuevas y emocionantes aventuras, conquista a tus enemigos y sobretodo supera tu propio ser!";
+				sWorld->SendServerMessage(SERVER_MSG_STRING, ss.str().c_str());
+				player->LearnSpell(54197, false);
+
+				if(player->getRace()==1 || player->getRace()==3 || player->getRace()==4 || player->getRace()==7 || player->getRace()==11){
+					//ALIZ
+					player->AddItem(12353,1);
+				}
+				else{
+					//Horda
+					player->AddItem(12330,1);
+				}
+				
+				player->AddItem(33223,1);
+				player->AddItem(50250,1);
+				player->AddItem(44794,1);
+				break;
+			}
+			 default:
+       return;
+		}
+		
+	}
+};
+
+
+class XpWeekend : public PlayerScript
+{
+    public:
+    XpWeekend() : PlayerScript("XpWeekend") { }
+    void OnGiveXP(Player* /*player*/, uint32& amount, Unit* /*victim*/)
+{
+    boost::gregorian::date date(boost::gregorian::day_clock::local_day());
+    auto day = date.day_of_week();
+    if (day == boost::date_time::Friday ||
+        day == boost::date_time::Saturday ||
+        day == boost::date_time::Sunday) {
+        amount = amount*2; }
+}
+    void OnLogin(Player* player, bool /*firstLogin*/)
+    {
+
+
+     boost::gregorian::date date(boost::gregorian::day_clock::local_day());
+	 auto day = date.day_of_week();
+	 if(day == boost::date_time::Friday ||
+        day == boost::date_time::Saturday ||
+        day == boost::date_time::Sunday) {
+        ChatHandler(player->GetSession()).PSendSysMessage("Doble experiencia activada!");
+        }
+    }
+};
+
+
 void AddSC_npc_promo()
 {
 	new npc_promo();
+	new custom_CongratsOnLevel();
+	new XpWeekend();
 }
